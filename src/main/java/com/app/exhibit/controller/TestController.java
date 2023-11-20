d8f47d1e11a8bc6eef238848c3ef7a3f82b0a45e
shinmirim <148671752+devVarier@users.noreply.github.com>
2023-11-20 22:24:41 +0900

    세팅완료
    2023.11.20 세팅완료

---
+++ src/main/java/com/app/exhibit/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.app.exhibit.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	
+	@GetMapping("/test/hello")
+	public String hello() {
+		return "hello";
+	}
+
+}
